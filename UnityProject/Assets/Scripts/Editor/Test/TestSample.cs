dd930deccbc8f44d4fc03e7eeda9b77bf995829c
MizoTake <mizotake123@gmail.com>
2020-02-04 08:33:51 +0900

    first

---
+++ UnityProject/Assets/Scripts/Editor/Test/TestSample.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+public class TestSample
+{
+    
+    [Test]
+    public void Passed()
+    {
+        Assert.AreEqual(true, true);
+    }
+    
+    [Test]
+    public void Failed()
+    {
+        Assert.AreEqual(true, false);
+    }
+}
